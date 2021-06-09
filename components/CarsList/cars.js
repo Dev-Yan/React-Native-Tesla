25d29115ad9260c7ff9f77d0a24c221468d61ac5
Yan Lima <71718541+Dev-Yan@users.noreply.github.com>
2021-06-09 09:14:57 -0300

    Tesla

---
+++ components/CarsList/cars.js
@@ -0,0 +1,21 @@
+export default [{
+    name: 'Model X',
+    tagline: 'A partir de $100,420',
+    image: require('../../assets/images/ModelX.jpeg'),
+  }, {
+    name: 'Model 3',
+    tagline: 'Encomende online para',
+    taglineCTA: 'Entrega sem toque',
+    image: require('../../assets/images/Model3.jpeg'),
+  }, {
+    name: 'Model S',
+    tagline: 'Encomende online para',
+    taglineCTA: 'Entrega sem toque',
+    image: require('../../assets/images/ModelS.jpeg'),
+    
+  }, {
+    name: 'Model Y',
+    tagline: 'Encomende online para',
+    taglineCTA: 'Entrega sem toque',
+    image: require('../../assets/images/ModelY.jpeg'),
+  }];
